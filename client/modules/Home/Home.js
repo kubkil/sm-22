7d91f42e33795383b28abac1e2df0e44cdb95596
Jakub Kilar <kuba.kilar@gmail.com>
2018-06-21 21:43:07 +0200

    import styles and add class to div

--- client/modules/Home/Home.js
+++ client/modules/Home/Home.js
@@ -3,12 +3,12 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 // Import Style
-// import styles from './Home.css';
+import styles from './Home.css';
 
 class Home extends Component {
   render() {
     return (
-      <div>
+      <div className={styles.header}>
         <h2>Hello world!</h2>
       </div>
     );
